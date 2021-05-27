dd0ad4546e14894949319e22ec44bd59b4dfa466
akshada <akshadasipsap@gmail.com>
2021-05-27 21:22:27 +0530

    login page updates

--- js/index.js
+++ js/index.js
@@ -4,28 +4,25 @@ loginButton.addEventListener('click',() => {
 });
 
 let login = () => {
-	let email = document.getElementById('email').value;
-	let password = document.getElementById('password').value;
-	if (email && password) {
-		fetch('https://jsonplaceholder.typicode.com/users',{
-			method: 'POST',
-			headers:{
-				'Content-Type' : 'application/json'
-			},
-			body : JSON.stringify({
-				email: email,
-				password : password
-			})
-		}).then(() => {
-			document.location.href = "userpage.html";
-			document.getElementById('loginform').reset();
-		});
+	document.location.href = "userpage.html";
+	// let email = document.getElementById('email').value;
+	// let password = document.getElementById('password').value;
+	// if (email && password) {
+	// 	fetch('https://jsonplaceholder.typicode.com/users',{
+	// 		method: 'POST',
+	// 		headers:{
+	// 			'Content-Type' : 'application/json'
+	// 		},
+	// 		body : JSON.stringify({
+	// 			email: email,
+	// 			password : password
+	// 		})
+	// 	}).then(() => {
+	// 		document.location.href = "userpage.html";
+	// 		document.getElementById('loginform').reset();
+	// 	});
 		
-	}
+	// }
 }
-
-// let loginRedirect = () => {
-// 	document.location.href = "userpage.html";
-// }
 
 let signUpButton = () => document.location.href = "signup.html"
